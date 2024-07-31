60f02fc307c9f892ec62cae17191689cc17ac1e3
minseogo <gominseo1112@gmail.com>
2024-07-31 15:06:55 +0900

    [1.1.4] vercel build error

--- src/component/pyj/quick/quickmenu.js
+++ src/component/pyj/quick/quickmenu.js
@@ -3,27 +3,27 @@ import React, {useEffect} from "react";
 
 
 const QuickMenu = () => {
-  useEffect(() => {
-    const handleToggleMenu = () => {
-      document.querySelector("#quick_pyj").classList.toggle("showmenu");
-    };
+  // useEffect(() => {
+  //   const handleToggleMenu = () => {
+  //     document.querySelector("#quick_pyj").classList.toggle("showmenu");
+  //   };
 
-    const quickPlusIcon = document.querySelector("#quick_pyj .quickplusicon");
-    quickPlusIcon.addEventListener("click", handleToggleMenu);
+  //   const quickPlusIcon = document.querySelector("#quick_pyj .quickplusicon");
+  //   quickPlusIcon.addEventListener("click", handleToggleMenu);
 
-    return () => {
-      quickPlusIcon.removeEventListener("click", handleToggleMenu);
-    };
-  }, []);
+  //   return () => {
+  //     quickPlusIcon.removeEventListener("click", handleToggleMenu);
+  //   };
+  // }, []);
     
     return (
       <aside id="quick_pyj">
         <div className='radishQuick'>
             <ul className='d-flex flex-column'>
-                <Quickitem></Quickitem>             
+                {/* <Quickitem></Quickitem>              */}
             </ul>
             <a href="#none" className='quickplusicon'>
-                <img src="/img/img_pyj/quick/plus.svg" alt="플러스" className='quickIcon' />
+                {/* <img src="/img/img_pyj/quick/plus.svg" alt="플러스" className='quickIcon' /> */}
             </a>
         </div>
       </aside>
